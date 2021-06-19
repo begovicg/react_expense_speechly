33e77bbf039f7decb66505350c07d79fdef8960e
creendev <developer@creen.io>
2021-06-20 00:39:57 +0200

    Added Details component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Details from './components/Details/Details';
 
 const App = () => {
     return (
         <div>
-            Expanse tracker app
+            <Grid container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+            </Grid>
         </div>
     )
 }
